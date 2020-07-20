3f771487b31387d4bdb4c331d425d33cc96d3c7f
Caroline Evans <caroline.evans@seas.upenn.edu>
2020-07-20 14:31:05 -0700

    feat: finished wrapping for server.route

--- plugins/node/opentelemetry-hapi-instrumentation/src/hapi.ts
+++ plugins/node/opentelemetry-hapi-instrumentation/src/hapi.ts
@@ -17,26 +17,67 @@
 import { BasePlugin } from '@opentelemetry/core';
 import * as Hapi from '@hapi/hapi';
 import { VERSION } from './version';
+import { HapiComponentName } from './types';
 
-// import * as shimmer from 'shimmer';
-
+import * as shimmer from 'shimmer';
 
 /** Hapi instrumentation for OpenTelemetry */
 export class HapiInstrumentation extends BasePlugin<typeof Hapi> {
-    static readonly component = 'hapi';
-  
-    constructor(readonly moduleName: string) {
-      super('@opentelemetry/koa-instrumentation', VERSION);
-    }
+  static readonly component = HapiComponentName;
 
+  constructor(readonly moduleName: string) {
+    super('@opentelemetry/hapi-instrumentation', VERSION);
+  }
 
-    protected patch(): typeof Hapi {
-        throw new Error("Method not implemented.");
+  protected patch(): typeof Hapi {
+    this._logger.debug('Patching hapi');
+    console.log('patching hapi');
+    if (this._moduleExports === undefined || this._moduleExports === null) {
+      return this._moduleExports;
     }
-    protected unpatch(): void {
-        throw new Error("Method not implemented.");
-    }
-  
+    shimmer.wrap(
+      this._moduleExports,
+      'server',
+      this._getServerPatch.bind(this)
+    );
+
+    return this._moduleExports;
+  }
+
+  protected unpatch(): void {
+    const serverProto = this._moduleExports.Server.prototype;
+
+    shimmer.unwrap(serverProto, 'route');
+  }
+
+  private _getServerPatch(
+    original: (options?: Hapi.ServerOptions) => Hapi.Server
+  ) {
+    const plugin = this;
+
+    return function server(this: Hapi.Server, opts?: Hapi.ServerOptions) {
+      const newServer = original.apply(this, [opts]);
+
+      shimmer.wrap(newServer, 'route', plugin._getServerRoutePatch.bind(this));
+
+      return newServer;
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any;
+  }
+
+  private _getServerRoutePatch(
+    original: (route: Hapi.ServerRoute | Hapi.ServerRoute[]) => void
+  ): (route: Hapi.ServerRoute | Hapi.ServerRoute[]) => void {
+    console.log('well then');
+    return function route(
+      this: Hapi.Server,
+      route: Hapi.ServerRoute | Hapi.ServerRoute[]
+    ): void {
+      console.log('test');
+      original.apply(this, [route]);
+    };
+  }
 }
 
-export const plugin = new HapiInstrumentation('koa');
+export const plugin = new HapiInstrumentation(HapiComponentName);
